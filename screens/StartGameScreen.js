e4cbcbf18db99cc06cc64d1d8facd0c2febf16f4
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-08-10 13:45:56 +0200

    Refactor StartGameScreen to apply styles to the input container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>RESET</PrimaryButton>
       <PrimaryButton>CONFIRM</PrimaryButton>
@@ -12,3 +12,18 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 4, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
