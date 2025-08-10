bf88dab852c6e26b2453a478cd4389dbfbc70028
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-08-10 13:33:56 +0200

    Add StartGameScreen and PrimaryButton components; remove initial placeholder in App

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { TextInput, View } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>RESET</PrimaryButton>
+      <PrimaryButton>CONFIRM</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
